1b11364c710fba777431103acbeb0a2ce60c182d
Yves Van Broekhoven <yvesvanbroekhoven@gmail.com>
2014-09-26 10:57:38 +0200

    Added all possible options to Vimeo markdown handler

--- lib/lalala/markdown/handlers/vimeo.rb
+++ lib/lalala/markdown/handlers/vimeo.rb
@@ -1,11 +1,16 @@
 class Lalala::Markdown::Handlers::Vimeo < Lalala::Markdown::Handlers::Base
 
   def initialize(options={})
-    options.assert_valid_keys(:width, :height)
+    options.assert_valid_keys(:color, :height, :show_badge, :show_byline, :show_portrait, :show_title, :width)
 
     options = {
-      width:  560,
-      height: 315
+      color: "ffffff",
+      height: 315,
+      show_badge: 0,
+      show_byline: 0,
+      show_portrait: 0,
+      show_title: 0,
+      width:  560
     }.merge(options)
 
     @options = options
@@ -23,7 +28,8 @@ class Lalala::Markdown::Handlers::Vimeo < Lalala::Markdown::Handlers::Base
       "",
       width:           @options[:width],
       height:          @options[:height],
-      src:             "//player.vimeo.com/video/#{id}",
+      src:             "//player.vimeo.com/video/#{id}?portrait=#{@options[:show_portrait]}&title=#{@options[:show_title]}&badge=#{@options[:show_badge]}&byline=#{@options[:show_byline]}&color=#{@options[:color]}&wmode=transparent",
+
       frameborder:     0,
       allowfullscreen: true
     )
