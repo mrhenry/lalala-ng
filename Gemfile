f07261fb42f7c9d8589c9037f0f75b8e1770664c
Pieter Beulque <pieterbeulque@gmail.com>
2015-12-17 11:13:04 +0100

    Update Lalala to 2.1.8

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby "2.1.7"
+ruby "2.1.8"
 
 gemspec name: 'lalala'
 gemspec name: 'lalala-development', groups: [:development]
--- lib/lalala/utils/install_template.rb
+++ lib/lalala/utils/install_template.rb
@@ -2,7 +2,7 @@ class AppBuilder < Rails::AppBuilder
   include Thor::Actions
   include Thor::Shell
 
-  RUBY_VERSION   = "2.1.7"
+  RUBY_VERSION   = "2.1.8"
   LALALA_VERSION = "4.1.0.dev"
 
   # You can test the generator locally by pointing to your local gems:
